87fac4140d95342fe9220947f5ba11166f1dced3
Stanislav <stas.kvizhinadze@bk.ru>
2023-01-20 00:26:11 +0300

    upd

---
+++ planner-todo/src/test/java/com/kviz/micro/planner/todo/PlannerTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kviz.micro.planner.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
