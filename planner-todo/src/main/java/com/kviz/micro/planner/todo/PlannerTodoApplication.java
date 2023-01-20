d6963c87233702225f68e128d14970bd9b4ed5e3
Stanislav <stas.kvizhinadze@bk.ru>
2023-01-21 01:03:00 +0300

    improved project: add controller, repository, aspect and service

--- planner-todo/src/main/java/com/kviz/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/com/kviz/micro/planner/todo/PlannerTodoApplication.java
@@ -3,9 +3,14 @@ package com.kviz.micro.planner.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan(basePackages = {"com.kviz.micro.planner"})
+@EnableJpaRepositories(basePackages = {"com.kviz.micro.planner.todo"})
 public class PlannerTodoApplication {
 
 	public static void main(String[] args) {
